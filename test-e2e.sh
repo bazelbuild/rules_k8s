d90f9754365065e55815744a14767a5cbd455b2a
Erick Fejta <fejta@google.com>
2018-07-02 15:30:52 -0700

    Create gcloud-bazel image for running e2e tests

--- test-e2e.sh
+++ test-e2e.sh
@@ -16,28 +16,63 @@
 
 set -o errexit
 set -o nounset
+set -o xtrace
+
+# Helper command for local debugging.
+# Note: prow jobs already run inside a container
+run-in-container() {
+  cd "$(dirname "${BASH_SOURCE}")"
+  echo "startup --host_jvm_args=-Duser.name=$USER" > /tmp/test-e2e.bazelrc
+  args=(
+    # Ensure we do not deploy to namespace root
+    # But don't use -u $(id -u) as this is a pain
+    -e "USER=$USER"
+    -v /tmp/test-e2e.bazelrc:/root/.bazelrc
+    # Map rules_k8s repo into /workspace
+    -v "$PWD":/workspace
+    -w /workspace
+    # Map in credentials
+    -v "$HOME/.kube":/root/.kube
+    -v "$HOME/.config/gcloud":/root/.config/gcloud
+    # Remove image after completion
+    --rm=true gcr.io/rules-k8s/gcloud-bazel:latest-"$USER"
+    # Run this script
+    "./$(basename "${BASH_SOURCE}")"
+  )
+
+  docker run "${args[@]}" "$@"
+  exit 0
+}
+
+if [[ "${1:-}" == "--container" ]]; then
+  shift
+  run-in-container "$@"
+fi
 
 check-plat() {
   local plat="$(uname -s)"
   if [[ $plat != Linux ]]; then
-	  echo "System must be Linux, found: $plat"
-	  return 1
+    echo "Consider using --container" >&2
+    echo "System must be Linux, found: $plat" >&2
+    return 1
   fi
 }
 
 check-plat
 
-if [[ -n "${HELLO_WORLD:-}" ]]; then
-  # Log into GCR
-  docker login -u _json_key -p "${GOOGLE_JSON_KEY}" https://us.gcr.io
+set +o xtrace
+if [[ -n "${GOOGLE_JSON_KEY:-}" ]]; then
   # Log into gcloud
   echo -n "${GOOGLE_JSON_KEY}" > keyfile.json
   gcloud auth activate-service-account --key-file keyfile.json
   rm -f keyfile.json
-  gcloud config set core/project rules-k8s
-  gcloud config set compute/zone us-central1-f
-  gcloud container clusters get-credentials testing
 fi
+set -o xtrace
+
+# Setup our credentials
+gcloud container clusters get-credentials testing --project=rules-k8s --zone=us-central1-f
+gcloud auth configure-docker --quiet
+
 # Check our installs.
 bazel version
 gcloud version
