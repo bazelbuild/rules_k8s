7d9fa2e7de627468e4af39995d6001b325dfedaa
Suvanjan Mukherjee <suvanjan@google.com>
2018-10-26 14:59:40 -0400

    Delete objects in namespace as well

--- test-e2e.sh
+++ test-e2e.sh
@@ -93,7 +93,10 @@ kubectl get "namespaces/${E2E_NAMESPACE}" || kubectl create namespace "${E2E_NAM
 delete() {
     # Delete the namespace
     echo "Deleting kubernetes namespace ${E2E_NAMESPACE}"
-    kubectl delete namespace "${E2E_NAMESPACE}"
+    # Delete everything in the namespace
+    kubectl delete --all --namespace=$E2E_NAMESPACE
+    # Delete the namespace
+    kubectl delete namespaces/$E2E_NAMESPACE
 }
 
 # Setup a trap to delete the namespace on error
