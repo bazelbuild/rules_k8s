8cc192415d66b0f3f4265faed1eb1fcada962081
Rohan Singh <rohan@washington.edu>
2020-01-21 16:54:27 -0500

    Extend and document substitution tests
    * Add some docstrings explaining what this test class is doing.
    
    * Test for the case of substituting a value that is not a plain YAML string.

--- examples/stamping/substitution_test.py
+++ examples/stamping/substitution_test.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""Tests that substitutions in k8s_object are applied correctly."""
 
 import os
 import subprocess
@@ -20,9 +21,11 @@ import yaml
 def TestData(name):
   return os.path.join(os.environ['TEST_SRCDIR'], 'io_bazel_rules_k8s', name)
 
-class DeploymentTest(unittest.TestCase):
+class SubstitutionTest(unittest.TestCase):
 
-  def test_username_substituted(self):
+  def test_e2e_namespace_substituted(self):
+    """Tests that the %{e2e_namespace} in the template is substituted with a
+    stamp variable."""
     k8s_obj = TestData('examples/stamping/substitution_stamping')
     out = subprocess.check_output([k8s_obj])
     generated = yaml.load(out)
@@ -31,6 +34,14 @@ class DeploymentTest(unittest.TestCase):
         generated['data']['e2e_namespace'],
         r"^build-.+$",
     )
+
+  def test_key_substituted(self):
+    """Tests that the %{key} in the template is substituted with a fixed string."""
+    k8s_obj = TestData('examples/stamping/substitution_stamping')
+    out = subprocess.check_output([k8s_obj])
+    generated = yaml.load(out)
+
+    self.assertEqual("bar", generated['data']['foo'])
 
 if __name__ == '__main__':
   unittest.main()
