0a9e6d75ca7e544a2fc3c6d770db01ba930d7f20
Suvanjan Mukherjee <suvanjan@google.com>
2020-02-25 13:44:57 -0500

    Mark stamper and reverser as python 3 (#531)
    * Mark stamper and reverser as python 3
    
    * Switch host force flags to python 3
    
    * buildifier

--- k8s/reverser.py
+++ k8s/reverser.py
@@ -13,6 +13,8 @@
 # limitations under the License.
 """Reverses the object order in a multi-document yaml."""
 
+from __future__ import absolute_import
+from __future__ import division
 from __future__ import print_function
 
 import argparse
--- k8s/stamper.py
+++ k8s/stamper.py
@@ -13,6 +13,9 @@
 # limitations under the License.
 """Resolve stamp variables."""
 
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
 import argparse
 import sys
 
